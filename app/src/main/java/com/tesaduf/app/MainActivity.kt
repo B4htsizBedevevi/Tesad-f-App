8b27229786506e5ec078f7899d07db6af29587d5
BahtsizBedevi <55953865+B4htsizBedevevi@users.noreply.github.com>
2026-09-13 02:14:35 +0300

    fix: clean Compose activity entry point

--- app/src/main/java/com/tesaduf/app/MainActivity.kt
+++ app/src/main/java/com/tesaduf/app/MainActivity.kt
@@ -3,13 +3,12 @@ package com.tesaduf.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.tesaduf.app.ui.TesadufApp
 import com.tesaduf.app.ui.TesadufViewModel
 import com.tesaduf.app.ui.theme.TesadufTheme
 
-class MainActivity: ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
