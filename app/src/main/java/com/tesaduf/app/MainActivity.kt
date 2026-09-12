eb76ba4e8ebea5f34a6c572af0f1d67433d0ae60
BahtsizBedevi <55953865+B4htsizBedevevi@users.noreply.github.com>
2026-09-13 01:59:46 +0300

    feat: add TESADUF app scaffold (app/src/main/java/com/tesaduf/app/MainActivity.kt)

---
+++ app/src/main/java/com/tesaduf/app/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.tesaduf.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tesaduf.app.ui.TesadufApp
+import com.tesaduf.app.ui.TesadufViewModel
+import com.tesaduf.app.ui.theme.TesadufTheme
+
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TesadufTheme {
+                val vm: TesadufViewModel = viewModel()
+                TesadufApp(vm)
+            }
+        }
+    }
+}
